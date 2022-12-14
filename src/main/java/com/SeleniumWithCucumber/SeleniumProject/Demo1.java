aa5805f56c188c4a7a82d5fd14798de7c4fe5e0a
sumeru1829 <sumeru1829@gmail.com>
2022-12-14 07:19:05 +0530

    creating seleniumProject

---
+++ src/main/java/com/SeleniumWithCucumber/SeleniumProject/Demo1.java
@@ -0,0 +1,14 @@
+package com.SeleniumWithCucumber.SeleniumProject;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		}
+		}
+
+		
+
+	
+
+
---
+++ src/test/java/DuplicateString.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateString {
+
+	public static void main(String[] args) {
+	String str="kondareddy";
+	int count;
+	char[] ch=str.toCharArray();
+	
+
+	for(int i=0;i<ch.length;i++){
+		count=1;
+		for(int j=i+1;j<ch.length;j++) {
+			
+			if(ch[i]==ch[j]) {
+				count++;
+				ch[j]='0';
+				
+			}
+		}
+		if(count>1 && ch[i]!='0') {
+			
+			System.out.println(ch[i]);
+		}
+	}
+
+	}
+
+}
